969c366498797ef018f04070bd4d38734ef92e2a
italiazin <centojunior@gmail.com>
2026-05-09 02:28:36 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -218,7 +218,7 @@ API_KEY = CONFIG.get("API_KEY", "")
 
 print("CONFIG PATH:", CONFIG_PATH)
 print("API KEY:", API_KEY)
-print("VERSAO_CARREGADA: OCR_CLIENTE_V145_SEM_INTERVALOS_VISUAIS")
+print("VERSAO_CARREGADA: OCR_CLIENTE_V146_SEM_OBSERVERS_VISUAIS")
 
 if os.name == "nt":
     tess_path = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
